e7b8d0f7768dc83a68a0726aefc59b1671df6e10
Jeff Muizelaar <jmuizelaar@mozilla.com>
2016-01-12 12:14:28 -0500

    Remove unneeded code.

--- gfxplatform.cc
+++ gfxplatform.cc
@@ -1,11 +1,5 @@
 #include "header.h"
 
-extern nsTHashtable<int> k;
-nsTHashtable<int> f()
-{
-	return k;
-}
-
 PLDHashTable::PLDHashTable(struct PLDHashTableOps const *) {}
 gfxPlatformFontList::gfxPlatformFontList() {
 }
