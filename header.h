08a2980eb8bcb30ed05f481306cefc2d974ff627
Jeff Muizelaar <jmuizelaar@mozilla.com>
2016-01-12 18:59:21 -0500

    Remove member

--- header.h
+++ header.h
@@ -6,20 +6,21 @@ class PLDHashTable {
 	public:   PLDHashTable(const PLDHashTableOps *aOps);
 };
 
-struct nsTHashtable {
-	   nsTHashtable()       : mTable(Ops()) {
-		  }
-	  PLDHashTable mTable;
-	  static const PLDHashTableOps *Ops();
-};
-
 inline void dancing() {}
 
-inline const PLDHashTableOps *nsTHashtable::Ops() {
+inline const PLDHashTableOps *Ops() {
 	static const PLDHashTableOps sOps = {
 		true ? 0 : dancing};
 	return &sOps;
 }
+
+struct nsTHashtable {
+	   nsTHashtable()       : mTable(Ops()) {
+		  }
+	  PLDHashTable mTable;
+};
+
+
 
 struct gfxPlatformFontList  {
 	gfxPlatformFontList();
