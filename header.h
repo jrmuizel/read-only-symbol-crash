2e887a3a8ce0acbf2a87355275bc25faef969503
Jeff Muizelaar <jmuizelaar@mozilla.com>
2016-01-13 13:31:35 -0500

    Add an explanatory comment

--- header.h
+++ header.h
@@ -4,6 +4,11 @@ const T *f(const T *);
 
 inline const T *Ops() {
 	static int k;
+	// variables initialized to expressions involving variables will not be put in rodata
+	// The following examples are put in rodata:
+	//  static const T sOps = &k;
+	//  static const T sOps = true ? 0 : 0;
+	// This one is not:
 	static const T sOps = true ? 0 : &k;
 	return &sOps;
 }
